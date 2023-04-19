7a6e5d3cc5cfe471e36600a73103f618bc270928
CatholicFootsteps <123822514+CatholicFootsteps@users.noreply.github.com>
2023-04-18 21:52:16 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const joinBtn = document.getElementById('join-btn');
+
+    joinBtn.addEventListener('click', () => {
+        window.location.href = 'registration.html';
+    });
+});
